fe4b778b35c31ee2781b5396ee0b738c296cb5b7
Eric <gutierrez.e_13@yahoo.com>
2023-03-14 02:44:56 +0100

    Created API function for code base.

---
+++ apikey.py
@@ -0,0 +1,9 @@
+from requests import *
+
+class DataQuery:
+    def query(ticker, function, interval):
+        apiKey = "VVYFBPRGDKHX0K93"
+        url = 'https://www.alphavantage.co/query?function=%s&symbol=%s&interval=%s&apikey=%s' % (function, ticker, interval, apiKey)
+        r = get(url)
+        data = r.json()
+        return data
