5cd06d695c3dcbe924ffaa43918c96b31edd6e1a
Nicholas Kapp <nic@Nicholass-MacBook-Air-2.local>
2023-03-17 19:37:32 -0500

    added chart type prompt

--- apikey.py
+++ apikey.py
@@ -3,6 +3,9 @@ from requests import *
 class DataQuery:
     def query(ticker, function, interval):
         apiKey = "VVYFBPRGDKHX0K93"
+        # Ask user for chart type
+        chart_type = input("Enter Chart Type (line/bar): ")
+        
         url = 'https://www.alphavantage.co/query?function=%s&symbol=%s&interval=%s&apikey=%s' % (ticker, function, interval, apiKey)
         r = get(url)
         data = r.json()
