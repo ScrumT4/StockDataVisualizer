b6426761fdfce3de1258e0f3e91f76d30b1e365f
Tdneubeck <89403889+Tdneubeck@users.noreply.github.com>
2023-03-20 08:54:42 -0500

    Update apikey.py

--- apikey.py
+++ apikey.py
@@ -1,9 +1,10 @@
 from requests import *
 
 class DataQuery:
+    stocksymbol()
     def query(ticker, function, interval):
         apiKey = "VVYFBPRGDKHX0K93"
-        url = 'https://www.alphavantage.co/query?function=%s&symbol=%s&interval=%s&apikey=%s' % (ticker, function, interval, apiKey)
+        url = 'https://www.alphavantage.co/query?function=%s&symbol=%s&interval=%s&apikey=%s' % (ticker, symbol, interval, apiKey)
         r = get(url)
         data = r.json()
         return data
