d407d5e44c16149ef60d48a12d6b76052b6e5274
Tdneubeck <89403889+Tdneubeck@users.noreply.github.com>
2023-03-20 09:00:10 -0500

    Update apikey.py

--- apikey.py
+++ apikey.py
@@ -1,6 +1,15 @@
 from requests import *
 
 class DataQuery:
+    def stocksymbol():
+        try:
+            print("Stock Data Visulaizer")
+            print("-------------------------")
+            global symbol  
+            symbol = input("Enter the symbol of the stock you would like to see:")
+        except ValueError:
+            print("Your input was invaild")
+
     stocksymbol()
     def query(ticker, function, interval):
         apiKey = "VVYFBPRGDKHX0K93"
