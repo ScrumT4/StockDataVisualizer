6f52cb05ae44a8ad2d644226ed6504a06da7115e
Nicholas Kapp <nic@Nicholass-MacBook-Air-2.local>
2023-03-21 18:08:17 -0500

    added chart type and create graph python file

---
+++ chartType.py
@@ -0,0 +1,26 @@
+import pygal
+
+class DataQuery:
+    def chartType(self):
+        #ask for Chart Type
+        try:
+            print("Chart Types")
+            print("-------------------------")
+            print("1. Bar")
+            print("2. Line")
+            self.chart_type = input("Enter the Chart Type you want (1, 2):")
+        except ValueError:
+            print("Your input was invaild(Enter '1' or '2')")
+
+    # Create graph based on user's selection
+    def createGraph(self, x_data, y_data):
+        # Create graph type
+        if self.chart_type == "1":
+            chart = pygal.Bar()
+        elif self.chart_type == "2":
+            chart = pygal.Line()
+        else:
+            print("Invalid chart type entered. Defaulting to line chart.")
+            chart = pygal.Line()
+
+
