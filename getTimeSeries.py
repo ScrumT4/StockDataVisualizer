3a110b7bcbf411e01e228111b594ce7b6f4e8e49
rxruiz <101907828+rxruiz@users.noreply.github.com>
2023-03-22 14:16:26 -0500

    change variable name
    adjusted variable name to match documentation

--- getTimeSeries.py
+++ getTimeSeries.py
@@ -2,8 +2,8 @@ def timeSeriesFunction():
     try:
         print("Select the Time Series of the chart you would like to generate\n------------------------------------------------")
         print("1. Intraday\n2. Daily\n3. Weekly\n4. Monthly")
-        global tsf
-        tsf = input("\nEnter Time Series option (1, 2, 3, 4): ")
+        global function
+        function = input("\nEnter Time Series option (1, 2, 3, 4): ")
     except ValueError:
         print("Invalid input. Please input 1, 2, 3, or 4")
 
