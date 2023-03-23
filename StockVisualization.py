23c30be5b750f18e25636b0ba4306db35d15e82d
Evan <98150291+wardenevanMU@users.noreply.github.com>
2023-03-23 15:15:39 -0500

    dates
    Ask the user for the beginning date in YYYY-MM-DD format.
    Ask the user for the end date in YYYY-MM-DD format.
    The end date should not be before the begin date

--- StockVisualization.py
+++ StockVisualization.py
@@ -75,3 +75,22 @@ def askTimeSeries(stockSymbol):
     r = requests.get(url)
     data = r.json()
     return data, timeSeries
+
+def checkDates():
+    format = "%Y-%m-%d"
+    while(True):
+        try:
+            startDate = input('\nEnter the start date (format: YYYY-MM-DD): ')
+            endDate = input('\nEnter the end date (format: YYYY-MM-DD): ')
+            datetime.strptime(startDate, format)
+            datetime.strptime(endDate, format)
+            if endDate < startDate:
+                print("End date must be after start date")
+                continue
+            break
+        except ValueError as e:
+            print("Incorrect start date format, should be YYYY-MM-DD")
+            continue
+        else:
+            break
+    return startDate, endDate
